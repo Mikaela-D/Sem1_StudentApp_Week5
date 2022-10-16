7862228b53585ed25a837f96e05340812edcf082
Mikaela-D <G00401392@atu.ie>
2022-10-16 21:16:22 +0100

    Prompt user for name, email and course

--- src/main/java/atu/ie/Student.java
+++ src/main/java/atu/ie/Student.java
@@ -3,5 +3,18 @@ package atu.ie;
 import java.util.Scanner;
 
 public class Student {
+    public static void main(String[] args) {
+        System.out.println("Please enter the student name");
+        Scanner stringScanner = new Scanner(System.in);
+        String name = stringScanner.next();
+        System.out.println("You entered : " + name);
 
+        System.out.println("Please enter the student email");
+        String email = stringScanner.next();
+        System.out.println("You entered : " + email);
+
+        System.out.println("Please enter the course name");
+        String course = stringScanner.next();
+        System.out.println("You entered : " + course);
+    }
 }
